24a534f9a3a18bd99a2af69a0bc9f5df894add8d
Martin Toman <whomenope@outlook.com>
2020-03-04 22:53:26 +0100

    Add dependencies, minimal setup

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,28 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import Lib
+import System.Environment (lookupEnv)
+import Control.Monad.Trans.Reader
+import Data.Text (pack)
+import qualified Web.Slack as Slack
+import qualified Web.Slack.Api as Api
+
+tokenEnvKey = "SLACK_TOKEN"
 
 main :: IO ()
-main = someFunc
+main = do
+  -- Try getting slack token from env
+  maybeToken <- lookupEnv tokenEnvKey
+  case maybeToken of
+    Nothing -> putStrLn "No slack token found in env."
+
+    -- If a token was found, start the app
+    Just token -> do
+      putStrLn "Got slack token."
+
+      slackConfig <- Slack.mkSlackConfig (pack token)
+
+      putStrLn "Running app"
+      _ <- flip runReaderT slackConfig (Slack.apiTest Api.mkTestReq)
+      putStrLn "Succesfully ran app"
